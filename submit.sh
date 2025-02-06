5894d5cc9035d975e3d8dc7a9453a376ea989997
Leon Leibmann <leon.leibmann@gmail.com>
2025-02-06 14:37:37 -0800

    Adjust submit.sh

--- submit.sh
+++ submit.sh
@@ -8,21 +8,21 @@ mkdir -p submit
 # submit team.txt
 printf "Leon Leibmann,lleibm\nNandini Talukdar,nandit\nKasten Welsh,kwelsh1" > submit/team.txt
 
-# train model (takes FOREVER)
-sudo ./container_run.sh python src/myprogram.py prepare --work_dir work
-sudo ./container_run.sh python src/myprogram.py train   --work_dir work
-
-# make predictions on example data submit it in pred.txt
-sudo ./container_run_testing.sh python src/myprogram.py test --work_dir work --test_data /job/data/example/input.txt --test_output submit/pred.txt
-
 # submit docker file
 cp Dockerfile submit/Dockerfile
 
 # submit source code
 cp -r src submit/src
 
-# submit checkpoints
-cp -r work submit/work
+# Make sure we can run testing script
+sudo ./container_run_testing.sh python src/myprogram.py test --work_dir work --test_data /job/data/example/input.txt --test_output submit/pred.txt
+
+# submit checkpoints, ignoring .parquet files
+mkdir -p submit/work
+rsync -av --exclude='*.parquet' work/ submit/work/
+
+# Submit files in root directory
+find . -maxdepth 1 -type f ! -lname '*' -exec cp {} submit/ \;
 
 # make zip file
 zip -r Project447Group3.zip submit

f873d4e23cff8be70d299fb8669f1b8ccb2beb57
Leon Leibmann <lleibm@klone-login03.hyak.local>
2025-02-06 00:01:42 -0800

    it fucking WORKSSS

--- run_hyak.sh
+++ run_hyak.sh
@@ -1,18 +1,46 @@
-#!/bin/bash
-#SBATCH --job-name=data-proc
-#SBATCH --partition=ckpt
-#SBATCH --account=stf
+#!/bin/bash -l
+#SBATCH --job-name=cse447-model-train
+#SBATCH --partition=ckpt-all
+#SBATCH --account=bdata
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=128G
 #SBATCH --gres=gpu:1
 #SBATCH --time=100:00:00
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=kwelsh1@uw.edu
+#SBATCH --mail-user=lleibm@uw.edu
 # I use source to initialize conda into the right environment.
+shopt -s expand_aliases
 source ~/.bashrc
-cat $0
+eval "$(micromamba shell hook --shell=bash)"
+
+
 echo "
 --------------------
+Setting Conda Env:
 "
-bash script.sh arg1 arg2
+conda env create -f environment.yml -y
+conda activate cse447
+
+echo "
+Env created!
+--------------------
+Checking for GPU...
+"
+
+python -c "import torch; print('CUDA is available.' if torch.cuda.is_available() else 'CUDA is not available.')"
+
+echo "
+--------------------
+Preparing Data...
+"
+
+python src/myprogram-nodocker.py prepare --work_dir work --data_dir /gscratch/scrubbed/gutenberg
+
+echo "
+Done preparing!
+--------------------
+Training...
+"
+
+python src/myprogram-nodocker.py train --work_dir work --data_dir /gscratch/scrubbed/gutenberg

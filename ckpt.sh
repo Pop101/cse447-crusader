9abef976e57da4e41c8dca746601e09288ca45fe
Kasten Welsh <welshkas000@gmail.com>
2025-02-05 21:24:41 -0800

    batch job

---
+++ ckpt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=data-proc
+#SBATCH --partition=ckpt
+#SBATCH --account=cse
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=128G
+#SBATCH --gres=gpu:4
+#SBATCH --time=100:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=kwelsh1@uw.edu
+# I use source to initialize conda into the right environment.
+source ~/.bashrc
+cat $0
+echo "
+--------------------
+"
+bash script.sh arg1 arg2

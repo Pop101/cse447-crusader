020abba308090b7045d0ecd1ff36cc83c8beb93d
Kasten Welsh <welshkas000@gmail.com>
2025-02-05 21:26:57 -0800

    stf

--- ckpt.sh
+++ ckpt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=data-proc
 #SBATCH --partition=ckpt
-#SBATCH --account=cse
+#SBATCH --account=stf
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=128G
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --time=100:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kwelsh1@uw.edu

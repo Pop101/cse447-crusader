a145127c4a2da9a7b4037353429b0abffa5bda8c
Leon Leibmann <leon.leibmann@gmail.com>
2025-02-10 02:04:34 -0800

    do not commit tensor pt files

--- submit.sh
+++ submit.sh
@@ -17,9 +17,9 @@ cp -r src submit/src
 # Make sure we can run testing script
 sudo ./container_run_testing.sh python src/myprogram.py test --work_dir work --test_data /job/data/example/input.txt --test_output submit/pred.txt
 
-# submit checkpoints, ignoring .parquet files
+# submit checkpoints, ignoring .parquet & tensor.pt files (intermediate data)
 mkdir -p submit/work
-rsync -av --exclude='*.parquet' work/ submit/work/
+rsync -av --exclude='*.parquet' --exclude='*_tensors_*.pt' work/ submit/work/
 
 # Submit files in root directory
 find . -maxdepth 1 -type f ! -lname '*' -exec cp {} submit/ \;

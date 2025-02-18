e1b4195fe8016e95d016f6e08b85fcceb42bdfd7
Leon Leibmann <leon.leibmann@gmail.com>
2025-02-17 19:29:32 -0800

    remove burning a batch

--- src/myprogram-nodocker.py
+++ src/myprogram-nodocker.py
@@ -150,9 +150,6 @@ if __name__ == '__main__':
                 #train_pairs       = create_random_length_sequence_pairs(train_pairs, 1, 100) # Create variable length sequences
                 train_pairs       = create_sequence_pairs(train_pairs, 100) # Create fixed length sequences
                 
-                x, y = next(train_pairs)
-                print(f"X: {x.shape}, Y: {y.shape}")
-                
                 loss = model.train_epoch(train_pairs)
                 print(f"Loss: {loss}")
                 # print(f"Best Loss: {model.best_loss}")

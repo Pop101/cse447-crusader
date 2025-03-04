287897f6dde1792eb460ea65f28338970f235222
Leon Leibmann <leon.leibmann@gmail.com>
2025-03-03 20:13:20 -0800

    8head

--- src/myprogram-nodocker.py
+++ src/myprogram-nodocker.py
@@ -152,7 +152,7 @@ if __name__ == '__main__':
                     model = TransformerPredictor.load(args.work_dir)
             else:
                 print('Instantiating Transformer Model')
-                model = TransformerPredictor(len(vocab), CHARS_PER_SAMPLE-1, embed_size=256, num_heads=4, num_layers=3)
+                model = TransformerPredictor(len(vocab), CHARS_PER_SAMPLE-1, embed_size=256, num_heads=8, num_layers=3)
         elif args.model == 'rnn':
             if os.path.exists(os.path.join(args.work_dir, 'RNNPredictor.pt')):
                 with TimerContext('Loading RNN Model'):
